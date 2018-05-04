0fa5c10d2925af1b3992f13e975d15df9311c51b
Ke Wang <453587854@qq.com>
2018-05-05 00:08:24 +0800

    naive implementation of find & unit tests

--- src/index.js
+++ src/index.js
@@ -1,5 +1,69 @@
-function find(html) {
-  return ["./hello.jpg"];
+function getAttrList(raw) {
+  let list = {};
+  let attrName = "";
+  let attrValue = "";
+  let state = 0; // 0 = looking for attr, 1 = looking for value
+  let quote = "";
+  for (let i = 0; i < raw.length; i++) {
+    let next = raw[i];
+    if (next === '=') {
+      if (state === 0) {
+        state = 1;
+        //next: find what quote it uses
+        i++;
+        if (raw[i] === `'`) {
+          quote = `'`;
+        } else if (raw[i] === `"`) {
+          quote = `"`;
+        } else {
+          quote = ` `; // space for no-quotes
+          i--;
+        }
+      } else if (state === 1) {
+        if (!quote) {
+          throw "unexpected =";
+        }
+        attrValue += next;
+      }
+    } else if (quote && next === quote) {
+      state = 0;
+      list [attrName] = attrValue;
+      // works too for "space for no-quotes"
+    } else if (next === ` `) {
+      if (state === 1) {
+        attrValue += next;
+      }
+    } else {
+      // default case for all remaining characters
+      if (state === 0) {
+        attrName += next;
+      } else if (state === 1) {
+        attrValue += next;
+      }
+    }
+  }
+  return list;
+}
+
+function find(html, settings = { tag: "img", attr: "src" }) {
+  const results = [];
+  const { tag = "img", attr = "src" } = settings;
+  let tagRegexp = new RegExp(`<${tag} (.+?)>`, "img");
+  let tagRegexpMatch = tagRegexp.exec(html);
+  let tagRegexpMatches = [];
+  while (tagRegexpMatch != null) {
+    tagRegexpMatches.push(tagRegexpMatch[1]);
+    tagRegexpMatch = tagRegexp.exec(html);
+  }
+  // tagRegexpMatches = [ 'src="./hello.jpg"', 'width=100 src="./hello.jpg"' ]
+  tagRegexpMatches.forEach(match => {
+    let attrList = getAttrList(match);
+    if (attrList[attr]) {
+      results.push(attrList[attr]);
+    }
+  });
+
+  return results;
 }
 
 module.exports = { find };
--- tests/find.js
+++ tests/find.js
@@ -4,6 +4,12 @@ const assert = chai.assert;
 
 describe("find", function () {
   it("should find all occurrences of src in an html string", function () {
-    assert.deepEqual(htmlFindSrc.find('...<img src="./hello.jpg"/>...'), ["./hello.jpg"]);
+    assert.deepEqual(htmlFindSrc.find('...<img src="./hello.jpg">abc</img>...<img width=100 src="./hello.jpg">abc</img>...'), ["./hello.jpg"]);
+  });
+  it("should be able to handle = in quotes", function () {
+    assert.deepEqual(htmlFindSrc.find('...<img src="./1=2.jpg"/>'), ["./1=2.jpg"]);
+  });
+  it("should be able to handle cases without quotes", function () {
+    assert.deepEqual(htmlFindSrc.find('...<img src=abc.jpg width=100/>'), ["abc.jpg"]);
   });
 });
